9fc086447786db3e7c654fda716d69806272db7c
Alozi <Alozimozi@gmail.com>
2023-01-31 16:38:00 +0200

    Fix bugs on home page

--- src/main.js
+++ src/main.js
@@ -6,4 +6,17 @@ import './css/main.scss';
 
 document.addEventListener('DOMContentLoaded', function () {
   window.console.log('Hello from main.js 👋.');
+
+  //Close mobile menu 
+  let mobileMenu = document.querySelector('#menu-drawer');
+  let menuDrawer = document.querySelector('#Details-menu-drawer-container');
+  console.log(menuDrawer);
+
+  window.onclick = function(event) {
+    if (event.target != mobileMenu) {
+      menuDrawer.classList.remove('menu-opening');
+      console.log('click');
+      // body.classList.remove('stop-scrolling');
+    }
+  }
 });
