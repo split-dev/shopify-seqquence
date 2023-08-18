43cee4aae51848ca60543f6807ca0026ccd9a297
Анастасия Найденко <anastasianajdenko@MBP-Anastasia.N4>
2023-08-18 13:36:55 +0300

    init new ui for product types

--- src/main.js
+++ src/main.js
@@ -16,4 +16,38 @@ document.addEventListener('DOMContentLoaded', function () {
       menuDrawer.classList.remove('menu-opening');
     }
   }
+
+  const dropdowns = document.querySelectorAll('.dropdown-toggle');
+  const dropdownItems = document.querySelectorAll('.dropdown-items [data-val]');
+
+  if (dropdowns?.length > 0) {
+    dropdowns.forEach((dr) => {
+      dr.addEventListener('click', (e) => {
+        e.currentTarget.classList.toggle('active');
+      });
+    });
+  }
+  if (dropdownItems?.length > 0) {
+    dropdownItems.forEach((di => {
+      di.addEventListener('click', (e) => {
+        const item = e.currentTarget;
+        const parentDropdown = item.closest('[data-target]');
+        const trgButtonSel = parentDropdown ? parentDropdown.getAttribute('data-target') : null;
+
+        parentDropdown.querySelectorAll('[data-val]').forEach((it) => {
+          it.classList.remove('active');
+        });
+        item.classList.add('active');
+
+        if (trgButtonSel) {
+          const trgButton = document.querySelector(trgButtonSel);
+          const trgLabel = trgButton ? trgButton.querySelector('.dropdown-label') : null;
+
+          trgLabel && (trgLabel.innerHTML = item.innerText);
+          trgButton && trgButton.classList.toggle('active');
+          trgButton.querySelector('input[type="hidden"]').value = item.getAttribute('data-val');
+        }
+      })
+    }))
+  }
 });
